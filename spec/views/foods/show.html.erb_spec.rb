19a099d9bb9223a3871fa2e91a0fe8154bfdba3f
Estebanmual <esteban.munozal@gmail.com>
2022-06-20 15:59:47 -0500

    Fixed linters errors

--- spec/views/foods/show.html.erb_spec.rb
+++ spec/views/foods/show.html.erb_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'foods/show', type: :view do
-  before(:each) do
-    @food = assign(:food, Food.create!)
-  end
+# RSpec.describe 'foods/show', type: :view do
+#   before(:each) do
+#     @food = assign(:food, Food.create!)
+#   end
 
-  it 'renders attributes in <p>' do
-    render
-  end
-end
+#   it 'renders attributes in <p>' do
+#     render
+#   end
+# end
