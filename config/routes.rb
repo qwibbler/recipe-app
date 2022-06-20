51427f4802d935a25fd020e8d03c5641b3b97b23
Estebanmual <esteban.munozal@gmail.com>
2022-06-20 13:22:53 -0500

    Add devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :recipes, only: [:index, :show, :create, :destroy, :new]
   resources :foods, only: [:index, :create, :destroy, :new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
