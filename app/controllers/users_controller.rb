e1cef632f4365c638cdf6ea9121eda14183cad62
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-22 13:49:00 -0600

    Make general_shopping_list for logged in users only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def shopping_list
-    @foods_list = current_user.missing_foods_qt
-    @total_price = 0
-    @foods_list.each { |food, qt| @total_price += food.price * qt }
+    if current_user
+      @foods_list = current_user.missing_foods_qt
+      @total_price = 0
+      @foods_list.each { |food, qt| @total_price += food.price * qt }
+    end
   end
 end
