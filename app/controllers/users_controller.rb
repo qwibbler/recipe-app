54e6bc3dd83ed22567eaa5ac73d1bae87d0ea9e3
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-22 14:17:12 -0600

    Linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def shopping_list
-    if current_user
-      @foods_list = current_user.missing_foods_qt
-      @total_price = 0
-      @foods_list.each { |food, qt| @total_price += food.price * qt }
-    end
+    return unless current_user
+
+    @foods_list = current_user.missing_foods_qt
+    @total_price = 0
+    @foods_list.each { |food, qt| @total_price += food.price * qt }
   end
 end
