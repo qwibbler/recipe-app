fdc02f24a4fa5eebc64c206756882ca1b9e18419
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-23 12:12:44 -0600

    Add test for recipefood_model

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @f1 = Food.create(name: 'Apple', measurement_unit: 'grams', price: 5)
+    r1 = Recipe.create(name: 'Recipe 1', public: false, description: 'desc')
+    @rf = RecipeFood.create(recipe: r1, food: @f1, quantity: 3)
+  end
+
+  it 'should return price' do
+    expect(@rf.total_price).to eq(15)
+  end
 end
