bb44a381f2478de21d368f0ffdffccbabb41efa0
Estebanmual <esteban.munozal@gmail.com>
2022-06-23 16:19:59 -0500

    Fix rubocop linters errors

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,55 +1,61 @@
 require 'rails_helper'
 RSpec.describe '/foods', type: :request do
-    let(:valid_params){ { food: {name:'apple', measurement_unit: 'unit', price:15} }}
-    let(:invalid_params){{food: {measurement_unit: 'unit', price:15} }}
+  let(:valid_params) { { food: { name: 'apple', measurement_unit: 'unit', price: 15 } } }
+  let(:invalid_params) { { food: { measurement_unit: 'unit', price: 15 } } }
 
-    it 'renders correctly index' do
-      get foods_path
-      expect(response).to have_http_status(:ok)
-      expect(response).to render_template(:index)
+  it 'renders correctly index' do
+    get foods_path
+    expect(response).to have_http_status(:ok)
+    expect(response).to render_template(:index)
+  end
+
+  it 'renders correctly new' do
+    get new_food_path
+    expect(response).to have_http_status(:ok)
+    expect(response).to render_template(:new)
+  end
+
+  describe 'Food / create' do
+    before(:each) do
+      post user_session_path,
+           params: { 'user' => { 'email' => 'john@example.com', 'password' => 'password', 'remember_me' => '0' } }
     end
-
-    it 'renders correctly new' do
-      get new_food_path
-      expect(response).to have_http_status(:ok)
-      expect(response).to render_template(:new)
-    end
-
-    describe 'Food / create' do
-      before(:each) {post user_session_path, params: {"user"=>{"email"=>"john@example.com", "password"=>"password", "remember_me"=>"0"}}}
-      context 'with valid parameters' do
-        it 'food / create' do
-          expect {post foods_path, params: valid_params}.to change(Food, :count).by(1)
-        end
-
-        it 'redirects to the root path after creating it' do
-          expect(response).to redirect_to('/')
-        end
+    context 'with valid parameters' do
+      it 'food / create' do
+        expect { post foods_path, params: valid_params }.to change(Food, :count).by(1)
       end
 
-      context 'with invalid parameters' do
-        it 'does not create a new food' do
-          expect {post foods_path, params: invalid_params}.to change(Food, :count).by(0)  
-        end
-        it 'renders a response with 422 status' do
-          post foods_path, params: invalid_params
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
+      it 'redirects to the root path after creating it' do
+        expect(response).to redirect_to('/')
       end
     end
 
-    describe 'food / delete' do
-      before(:each) {post user_session_path, params: {"user"=>{"email"=>"john@example.com", "password"=>"password", "remember_me"=>"0"}}}
-      it 'destroys the requested food' do
-        post foods_path, params: valid_params
-        food = Food.last
-        expect {delete food_path(food.id)}.to change(Food, :count).by(-1)
+    context 'with invalid parameters' do
+      it 'does not create a new food' do
+        expect { post foods_path, params: invalid_params }.to change(Food, :count).by(0)
       end
-      it 'redirects to root' do
-        post foods_path, params: valid_params
-        food = Food.last
-        delete food_path(food.id)
-        expect(response).to redirect_to('/foods')
+      it 'renders a response with 422 status' do
+        post foods_path, params: invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
+  end
+
+  describe 'food / delete' do
+    before(:each) do
+      post user_session_path,
+           params: { 'user' => { 'email' => 'john@example.com', 'password' => 'password', 'remember_me' => '0' } }
+    end
+    it 'destroys the requested food' do
+      post foods_path, params: valid_params
+      food = Food.last
+      expect { delete food_path(food.id) }.to change(Food, :count).by(-1)
+    end
+    it 'redirects to root' do
+      post foods_path, params: valid_params
+      food = Food.last
+      delete food_path(food.id)
+      expect(response).to redirect_to('/foods')
+    end
+  end
 end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 RSpec.describe '/recipes', type: :request do
-  let(:valid_params){{recipe: {name: 'Apple pay', description: 'Delicious dessert'}}}
-  let(:invalid_params){{recipe: {description: 'Delicious dessert'}}}
-  before(:each) {post user_session_path, params: {"user"=>{"email"=>"john@example.com", "password"=>"password", "remember_me"=>"0"}}}
+  let(:valid_params) { { recipe: { name: 'Apple pay', description: 'Delicious dessert' } } }
+  let(:invalid_params) { { recipe: { description: 'Delicious dessert' } } }
+  before(:each) do
+    post user_session_path,
+         params: { 'user' => { 'email' => 'john@example.com', 'password' => 'password', 'remember_me' => '0' } }
+  end
   it 'renders correctly index' do
     get recipes_path
     expect(response).to have_http_status(:ok)
     expect(response).to render_template(:index)
   end
-  
+
   it 'renders correctly new' do
     get new_recipe_path
     expect(response).to have_http_status(:ok)
@@ -18,7 +21,7 @@ RSpec.describe '/recipes', type: :request do
   describe 'recipes / create' do
     context 'with valid parameters' do
       it 'recipe create' do
-        expect {post recipes_path, params: valid_params}.to change(Recipe, :count).by(1)
+        expect { post recipes_path, params: valid_params }.to change(Recipe, :count).by(1)
       end
       it 'redirects to recipes index' do
         expect(response).to redirect_to('/')
@@ -26,7 +29,7 @@ RSpec.describe '/recipes', type: :request do
     end
     context 'with invalid parameters' do
       it 'does not create a new recipe' do
-        expect {post recipes_path, params: invalid_params}.to change(Recipe, :count).by(0)  
+        expect { post recipes_path, params: invalid_params }.to change(Recipe, :count).by(0)
       end
       it 'renders a response with 422 status' do
         post recipes_path, params: invalid_params
@@ -39,7 +42,7 @@ RSpec.describe '/recipes', type: :request do
     it 'destroys the requested recipe' do
       post recipes_path, params: valid_params
       recipe = Recipe.last
-      expect {delete recipe_path(recipe.id)}.to change(Recipe, :count).by(-1)
+      expect { delete recipe_path(recipe.id) }.to change(Recipe, :count).by(-1)
     end
     it 'redirects to root' do
       post recipes_path, params: valid_params
