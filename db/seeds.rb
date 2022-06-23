bb5cd43fa30c8e7aafd20863f94fdf71b457b800
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-23 12:05:55 -0600

    Add test for user_model

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ u2 = User.create(name: 'Jane', email: 'jane@example.com', password: 'password')
 f1 = Food.create(name: 'Apple', measurement_unit: 'grams', price: 5, user: u1)
 f2 = Food.create(name: 'Pineapple', measurement_unit: 'grams', price: 1, user: u1)
 f3 = Food.create(name: 'Chicken breasts', measurement_unit: 'unit', price: 2, user: u2)
-f3 = Food.create(name: 'Sugar', measurement_unit: 'cups', price: 7, user: u2)
+f4 = Food.create(name: 'Sugar', measurement_unit: 'cups', price: 7, user: u2)
 
 r1 = Recipe.create(name: 'Recipe 1', public: false, user: u1,
                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -15,6 +15,9 @@ r2 = Recipe.create(name: 'Recipe 2', public: true, user: u1,
 r3 = Recipe.create(name: 'Recipe 3', public: true, user: u1,
                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua.')
+
+r1f3 = RecipeFood.create(recipe: r1, food: f3, quantity: 3)
+r1f4 = RecipeFood.create(recipe: r1, food: f4, quantity: 3)
 
 r2f1 = RecipeFood.create(recipe: r2, food: f1, quantity: 3)
 r2f2 = RecipeFood.create(recipe: r2, food: f2, quantity: 3)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(
+      name: 'Tom Sawyer',
+      email: 'user1@example.com',
+      password: 'password'
+    )
+  end
+
+  before(:example) { subject.save }
+
+  it 'is valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'validates name presence' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'validates name length' do
+    subject.name = 't'
+    expect(subject).to_not be_valid
+  end
+
+  it 'validates email presence' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'validates password presence' do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'validates password length' do
+    subject.password = 't'
+    expect(subject).to_not be_valid
+  end
+
+  describe 'checks method missing_foods_qt' do
+    before do
+      u2 = User.new(name: 'test2', email: 'test2@example.com', password: 'password', created_at: Time.now)
+
+      f1 = Food.create(name: 'Apple', measurement_unit: 'grams', price: 5, user: subject)
+      @f2 = Food.create(name: 'Chicken', measurement_unit: 'unit', price: 2, user: u2)
+
+      r1 = Recipe.create(name: 'Recipe 1', public: false, user: subject, description: 'desc')
+      r2 = Recipe.create(name: 'Recipe 2', public: true, user: subject, description: 'desc')
+
+      RecipeFood.create(recipe: r1, food: @f2, quantity: 3)
+
+      RecipeFood.create(recipe: r2, food: f1, quantity: 3)
+      RecipeFood.create(recipe: r2, food: @f2, quantity: 3)
+    end
+
+    it 'should have 1 item' do
+      expect(subject.missing_foods_qt.count).to eq(1)
+    end
+
+    it 'should have be the food created by another user' do
+      expect(subject.missing_foods_qt.first[0]).to eq(@f2)
+    end
+
+    it 'should have the quantity 6' do
+      expect(subject.missing_foods_qt.keys[0]).to eq(@f2)
+    end
+  end
 end
