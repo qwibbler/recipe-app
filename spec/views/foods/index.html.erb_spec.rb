ed5ddcddc9ea3e38b4754a308b6b867630e84553
Estebanmual <esteban.munozal@gmail.com>
2022-06-23 10:07:06 -0500

    Added testings

--- spec/views/foods/index.html.erb_spec.rb
+++ spec/views/foods/index.html.erb_spec.rb
@@ -1,15 +1,23 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'foods/index', type: :view do
-#   before(:each) do
-#     assign(:foods, [
-#              Food.create!,
-#              Food.create!
-#            ])
-#   end
+RSpec.describe 'foods/index', type: :feature do
+  before(:each) do
+    visit new_user_session_path
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+  end
 
-#   it 'renders a list of foods' do
-#     render
-#     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-#   end
-# end
+  it 'renders a list of foods' do
+    expect(page).to have_selector('table.foodIndex_table')
+  end
+
+  it 'renders the navbar' do
+    expect(page).to have_content('Recipe App')
+  end
+
+  it 'when I click add food it redirects me to food new view' do
+    click_on "Add food"
+    expect(current_path).to eq('/foods/new')
+  end
+end
