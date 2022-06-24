9d28c62b1dc9489d444a51ff9bf86036ecbd1ab2
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-06-23 20:15:50 -0600

    Add Delete food only if user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,10 @@ class Ability
 
   def initialize(user)
     can :read, Recipe, public: true
+    can :read, Food
     return unless user.present?
 
-    can :manage, Recipe, user:
+    can :manage, Recipe, user: user
+    can :manage, Food, user:
   end
 end
